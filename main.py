1574b8c5d14ff130fa761e4b8e9f6bc771414beb
Collin Hughes <chughes2@binghamton.edu>
2017-04-02 12:54:57 -0400

    Puts all game data into game Class

--- main.py
+++ main.py
@@ -1,53 +1,64 @@
-# Pygame template
-import pygame
+#Imports
+import pygame as pg
 import random
+from settings import *
+
+class Game:
+    def __init__(self):
+        # Initialize game window
+        pg.init()
+        pg.mixer.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption("My Game")
+        self.clock = pg.time.Clock()
+        self.running = True
+
+    def new(self):
+        # Start a New Game
+        self.all_sprites = pg.sprite.Group()
+        self.run()
+
+    def run(self):
+        # Game loop
+        self.playing = True
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
+
+    def update(self):
+        # Game loop - Update
+        self.all_sprites.update()
+
+    def events(self):
+        # Game loop - Events
+        for event in pg.event.get():
+            # Check for closing the window
+            if event.type == pg.QUIT:
+                self.playing = False
+                self.running = False
+
+    def draw(self):
+        # Game loop - Draw
+        self.screen.fill(BLACK)
+        self.all_sprites.draw(self.screen)
+        # *after* drawing everything, flip the display
+        pg.display.flip()
+
+    def show_start_screen(self):
+        # Game splash/start screen
+        pass
+
+    def show_go_screen(self):
+        # Game over/continue
+        pass
 
 def main():
-    # Defines the screen size and times per second the game updates
-    WIDTH = 360
-    HEIGHT = 480
-    FPS = 30
-
-    # define basic debug colors
-    WHITE = (255, 255, 255)
-    BLACK = (0, 0, 0)
-    RED = (255, 0, 0)
-    GREEN = (0, 255, 0)
-    BLUE = (0, 0, 255)
-
-    # Creates a group that all sprites in the game must be added to
-    # Used for updates
-    all_sprites = pygame.sprite.Group()
-
-    # Initialize pygame as well as sound capabilities and create window
-    pygame.init()
-    pygame.mixer.init()
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption("My Game")
-    clock = pygame.time.Clock()
-
-    # Game Loop
-    running = True
-    while running:
-        # Keeps loop running at the right speed
-        clock.tick(FPS)
-        # Process input (events)
-        for event in pygame.event.get():
-            # check for closing the window
-            if event.type == pygame.QUIT:
-                running = False
-
-        # Update
-        all_sprites.update()
-
-        # Render
-        screen.fill(BLACK)
-        all_sprites.draw(screen)
-        # *after* drawing everything, flip the display
-        # This makes the game run faster as its not updating each pixel
-        # Instead it updates everything "behind" the screen and then shows it
-        pygame.display.flip()
-
-    pygame.quit()
+    g = Game()
+    g.show_start_screen()
+    while g.running:
+        g.new()
+        g.show_go_screen()
 
 main()
