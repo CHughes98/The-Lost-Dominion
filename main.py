6af137ec1ffdfdb36ae429a8c9e7446ecdf80a9a
Collin Hughes <chughes2@binghamton.edu>
2017-04-01 21:59:57 -0400

    Put loop into a function

--- main.py
+++ main.py
@@ -2,44 +2,47 @@
 import pygame
 import random
 
-WIDTH = 360
-HEIGHT = 480
-FPS = 30
+def main():
+    WIDTH = 360
+    HEIGHT = 480
+    FPS = 30
 
-# define colors
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
+    # define colors
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+    RED = (255, 0, 0)
+    GREEN = (0, 255, 0)
+    BLUE = (0, 0, 255)
 
-all_sprites = pygame.sprite.Group()
+    all_sprites = pygame.sprite.Group()
 
-# Initialize pygame and create window
-pygame.init()
-pygame.mixer.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("My Game")
-clock = pygame.time.Clock()
+    # Initialize pygame and create window
+    pygame.init()
+    pygame.mixer.init()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    pygame.display.set_caption("My Game")
+    clock = pygame.time.Clock()
 
-# Game Loop
-running = True
-while running:
-    # keep loop running at the right speed
-    clock.tick(FPS)
-    # Process input (events)
-    for event in pygame.event.get():
-        # check for closing the window
-        if event.type == pygame.QUIT:
-            running = False
+    # Game Loop
+    running = True
+    while running:
+        # keep loop running at the right speed
+        clock.tick(FPS)
+        # Process input (events)
+        for event in pygame.event.get():
+            # check for closing the window
+            if event.type == pygame.QUIT:
+                running = False
 
-    # Update
-    all_sprites.update()
+        # Update
+        all_sprites.update()
 
-    # Render
-    screen.fill(BLACK)
-    all_sprites.draw(screen)
-    # *after* drawing everything, flip the display
-    pygame.display.flip()
+        # Render
+        screen.fill(BLACK)
+        all_sprites.draw(screen)
+        # *after* drawing everything, flip the display
+        pygame.display.flip()
 
-pygame.quit()
+    pygame.quit()
+    
+main()
