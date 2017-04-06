90899e0b52781ff681fa79a36fe003352e14ff29
Collin Hughes <chughes2@binghamton.edu>
2017-04-06 08:22:28 -0400

    Most recent attempts at troubleshooting

--- sprites.py
+++ sprites.py
@@ -2,13 +2,6 @@ import pygame
 import random
 import settings
 
-def draw_text(surf, text, size, x, y):
-    font = pygame.font.Font(font_name, size)
-    text_surface = font.render(text, True, WHITE)
-    text_rect = text_surface.get_rect()
-    text_rect.midtop = (x, y)
-    surf.blit(text_surface, text_rect)
-
 class Player(pygame.sprite.Sprite):
     def __init__(self, x_start, y_start, health = 100, armor = 0, multiplier = 1):
         pygame.sprite.Sprite.__init__(self)
